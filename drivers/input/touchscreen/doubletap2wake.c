52021293eeff425b6fc48ceeb5f743ab99ecae4f
DarkAngelGR <asavvo01@gmail.com>
2019-05-17 14:00:26 +0000

    Revert "input: dt2w/s2w: register to ft5336 ts driver."
    This reverts commit 31057450c5e80da5305faf865bb2aec2eb5beae6.

--- drivers/input/touchscreen/doubletap2wake.c
+++ drivers/input/touchscreen/doubletap2wake.c
@@ -219,8 +219,7 @@ static void dt2w_input_event(struct input_handle *handle, unsigned int type,
 
 static int input_dev_filter(struct input_dev *dev) {
 	if (strstr(dev->name, "touch") ||
-		strstr(dev->name, "synaptics_dsx_i2c") ||
-		strstr(dev->name, "ft5x06_720p")) {
+		strstr(dev->name, "synaptics_dsx_i2c")) {
 		return 0;
 	} else {
 		return 1;
