11c014e26c185b676b228d2b5fb9841e88f96547
emabertax <47644322+emabertax@users.noreply.github.com>
2019-07-02 20:29:41 +0200

    CPU boost toggle
    added a toggle for CPU boost. This can switch on/off easy and fast for users without changing the configurations
    
    Thanks to Francisco Franco
    original commit taken here: https://github.com/droideronline/android_kernel_xiaomi_msm8937-custom/commit/88786459d59e03da4ceb0f24f4c86750bfea0294

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -33,7 +33,9 @@ struct cpu_sync {
 
 static DEFINE_PER_CPU(struct cpu_sync, sync_info);
 
-static unsigned int input_boost_enabled = 1;
+static struct work_struct input_boost_work;
+
+static unsigned int input_boost_enabled = 0;
 module_param(input_boost_enabled, uint, 0644);
 
 static unsigned int input_boost_ms = 500;
